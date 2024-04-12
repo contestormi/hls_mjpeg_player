8c1a8cd79eba97670a8e3746862006b26bec3630
contestormi <anssirod@gmail.com>
2024-04-12 12:18:52 +0300

    fixed black screen and infinity loading

--- ios/Classes/HlsMjpegPlayerPlugin.swift
+++ ios/Classes/HlsMjpegPlayerPlugin.swift
@@ -39,7 +39,7 @@ class HlsMjpegPlayer: NSObject, FlutterPlatformView, WKNavigationDelegate {
     private var mainView = HlsMjpegPlayerView()
     private var mChannel: FlutterMethodChannel?
     private var resultHandler: FlutterResult?
-    private var htmlContent: String = """
+    let htmlContent: String = """
 <html style="height: 100%; width: 100%;">
 <body style="margin: 0px;
              height: 100%;
@@ -65,11 +65,11 @@ class HlsMjpegPlayer: NSObject, FlutterPlatformView, WKNavigationDelegate {
         initVar(registrar: registrar)
         loadUrl(arguments: args)
     }
-    
+
     func view() -> UIView {
         return mainView
     }
-    
+
     deinit{
         mainView.webView.stopLoading()
         DispatchQueue.main.async {
@@ -78,49 +78,51 @@ class HlsMjpegPlayer: NSObject, FlutterPlatformView, WKNavigationDelegate {
                                                     modifiedSince: Date(timeIntervalSince1970: 0), completionHandler:{})
         }
     }
-    
+
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         let isBlank = webView.url?.absoluteString.contains("blank") ?? false
         if isBlank { return }
         let arg: [String: String?] = ["status": "Error"]
         mChannel?.invokeMethod("onStatusChange", arguments: arg)
     }
-    
+
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         let isBlank = webView.url?.absoluteString.contains("blank") ?? false
         if isBlank { return }
         let arg: [String: String?] = ["status": "Error"]
         mChannel?.invokeMethod("onStatusChange", arguments: arg)
     }
-    
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+
+    func webView(
+        _ webView: WKWebView,
+        didFinish navigation: WKNavigation!
+    ){
         let isBlank = webView.url?.absoluteString.contains("blank") ?? false
         let arg: [String: String?] = ["status": isBlank ? "Pause" : "Play"]
-        mChannel?.invokeMethod("onStatusChange", arguments: arg)
         mainView.imageView.isHidden = !isBlank
+        mChannel?.invokeMethod("onStatusChange", arguments: arg)
         if let resultHandler = resultHandler {
             resultHandler("Loaded")
         }
     }
-    
+
     private func loadUrl(arguments args: Any?){
         let arg: [String: String?] = ["status": "Loading"]
         mChannel?.invokeMethod("onStatusChange", arguments: arg)
-        
+
         let argumentsDictionary = args as? Dictionary<String, Any> ?? [:]
         let initialUrl = argumentsDictionary["url"] as? String ?? ""
-        
+
         let url = URL(string: initialUrl)
         if(!initialUrl.isEmpty && url != nil){
-            mainView.webView.loadHTMLString(self.htmlContent.replacingOccurrences(of: "#URL#", with: initialUrl),baseURL: url)
-          //  mainView.webView.load(URLRequest(url: url!))
+             mainView.webView.loadHTMLString(self.htmlContent.replacingOccurrences(of: "#URL#", with: initialUrl),baseURL: url)
         }
     }
-    
+
     private func pause(){
         mainView.pause()
     }
-    
+
     private func initVar(registrar: FlutterPluginRegistrar){
         mChannel = FlutterMethodChannel(name: "com.example.hls_mjpeg_player", binaryMessenger: registrar.messenger())
         mChannel?.setMethodCallHandler{ [weak self] call, result in
@@ -138,46 +140,53 @@ class HlsMjpegPlayer: NSObject, FlutterPlatformView, WKNavigationDelegate {
     }
 }
 
-private class HlsMjpegPlayerView: UIView{
+private class HlsMjpegPlayerView: UIView {
     let imageView = UIImageView()
-    let webView = WKWebView()
-    
+    let webView: WKWebView
+
     override init(frame: CGRect) {
+        let webConfiguration = WKWebViewConfiguration()
+        // Отключаем обнаружение номеров телефонов
+        webConfiguration.dataDetectorTypes = []
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+
         super.init(frame: frame)
+
+        webView.isOpaque = false
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.scrollView.isUserInteractionEnabled = false
         webView.scrollView.isScrollEnabled = false
-        
+
         imageView.contentMode = .scaleToFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         addSubview(webView)
         addSubview(imageView)
         addLayoutConstraint()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
     }
-    
+
     private func addLayoutConstraint() {
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: topAnchor),
             imageView.leftAnchor.constraint(equalTo: leftAnchor),
             imageView.rightAnchor.constraint(equalTo: rightAnchor),
             imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            
+
             webView.topAnchor.constraint(equalTo: topAnchor),
             webView.leftAnchor.constraint(equalTo: leftAnchor),
             webView.rightAnchor.constraint(equalTo: rightAnchor),
             webView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
-    
-    func pause(){
+
+    func pause() {
         let isBlank = webView.url?.absoluteString.contains("blank") ?? false
         if isBlank { return }
-        
+
         let config = WKSnapshotConfiguration()
         config.rect = webView.frame
         webView.takeSnapshot(with: config, completionHandler: { (image: UIImage?, error: Error?) in
@@ -187,4 +196,3 @@ private class HlsMjpegPlayerView: UIView{
         })
     }
 }
-
